0c56a76fc1cb734172b18089034be843d1d85d75
TheJohny71 <jduval71@me.com>
2024-12-04 16:09:54 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from '@/components/theme/theme-provider'
+import Layout from '@/components/layout/layout'
+
+const queryClient = new QueryClient()
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="system" storageKey="alfie-ui-theme">
+        <Router>
+          <Layout />
+        </Router>
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
+}
+
+export default App
