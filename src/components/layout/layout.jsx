a891bd099fe12fba3288b3d1bd23799a5d446305
TheJohny71 <jduval71@me.com>
2024-12-04 15:46:03 -0600

    Create layout.jsx

---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,16 @@
+import { Navbar } from "./navbar"
+import { Sidebar } from "./sidebar"
+
+export function Layout({ children }) {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}
