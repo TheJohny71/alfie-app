dca0f5081faf30767862287f4b0c77c4a6567a70
TheJohny71 <jduval71@me.com>
2024-12-04 16:43:42 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ThemeProvider } from '@/components/theme/theme-provider'
-import Layout from '@/components/layout/layout'
+import { ThemeProvider } from './components/theme/theme-provider'
+import Layout from './components/layout/layout'
 
 const queryClient = new QueryClient()
 
