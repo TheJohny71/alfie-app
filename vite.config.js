e2fd654b8988a1c7e0c4d6ecb773f03c778ae939
TheJohny71 <jduval71@me.com>
2024-12-04 16:43:18 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/alfie-app/'
+  base: '/alfie-app/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
