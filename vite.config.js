d50322f7f30025dc2203447ec29134f3920315d5
TheJohny71 <jduval71@me.com>
2024-12-04 17:59:13 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,10 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
+      external: [],
+      output: {
+        manualChunks: undefined
       }
     }
   }
